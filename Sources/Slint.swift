14a80d36e873856c2a6df81076c0318ae77e261e
Matthew Taylor <matthew@illucid.org>
2024-02-14 17:08:53 -0600

    Moved idle into its own function

--- Sources/Slint.swift
+++ Sources/Slint.swift
@@ -27,7 +27,7 @@ public protocol SlintApp {
 public extension SlintApp {
     /// Default implementation for `main()`, starting the event loop and concurrently running `start()`.
     static func main() async {
-        
+
         // Task to run `start()`
         Task {
             // Wait for the event loop to be ready before proceding.
@@ -43,22 +43,34 @@ public extension SlintApp {
         }
 
         // Task to make the damn thing process timers
-        let idleTask = Task {
-            await EventLoop.ready
-
-            while !Task.isCancelled {
-                // Sleep for 1ms
-                try! await Task.sleep(nanoseconds: 1_000_000)
-
-                // Make Slint do something
-                await { @SlintActor in
-                    slint_platform_update_timers_and_animations()
-                }()
-            }
-        }
+        let idleTask = Task { await idle() }
 
         // Start the event loop. This run on the main actor and block (basically) forever.
         await EventLoop.start()
         idleTask.cancel()
+    }
+}
+
+/// Idle task. Makes Slint update its state, then sleeps for as long as Slint thinks it should.
+/// Or 100ms. Whichever is shorter.
+func idle() async {
+    await EventLoop.ready
+
+    while !Task.isCancelled {
+        // How long until the next update?
+        var durationUntilUpdate: UInt64 = 1
+
+        await { @SlintActor in
+            // Make Slint do something
+            slint_platform_update_timers_and_animations()
+        
+            // How long until we should update again?
+            durationUntilUpdate = slint_platform_duration_until_next_timer_update()
+        }()
+
+        // Wait that duration or 100ms, whichever is less.
+        // Slint will set the value to UInt64.max if there is nothing waking it up.
+        durationUntilUpdate = durationUntilUpdate > 100 ? 100 : durationUntilUpdate
+        try! await Task.sleep(nanoseconds: durationUntilUpdate * 1_000_000)
     }
 }
